155b92d4eb131bbe5c20e4b664c0db50bf499e3e
badsectortv <headcrash@badsector.tv>
2016-10-12 20:22:37 +0100

    Some more layout gubbins. Added some logic to the post wrapper that checks at each state to see if there's something to even bother with. Should lbe a bit more robust now.

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,71 +1,60 @@
 window.onload = function() {
   var doc = document;
 
-  // JS… get in the sea
-  // Sensible precautions courtesy of James Edwards
-  // https://www.sitepoint.com/removing-useless-nodes-from-the-dom/
-  function clean(node)
-  {
-    for(var n = 0; n < node.childNodes.length; n ++)
-    {
-      var child = node.childNodes[n];
-      if
-      (
-        child.nodeType === 8
-        ||
-        (child.nodeType === 3 && !/\S/.test(child.nodeValue))
-      )
-      {
-        node.removeChild(child);
-        n --;
-      }
-      else if(
-        child.nodeType === 1 &&
-        (!child.tagName.toLowerCase() === "code"))
-      {
-        clean(child);
-      }
-    }
-  };
-  clean(doc.body);
-
   var prepareStage = function() {
-    var headers = doc.getElementsByTagName("h1"),
-        header = headers[0]; // only pick the first one from the page just in case
-                             // there's more… there shouldn't be, we'll write it
-                             // in the submissions guideline
-        subtitle = header.nextSibling,
-        masthead = subtitle.nextSibling,
-        wrapper = doc.createElement("div");
-    var fuckYouIAmInCharge = function(){
-      var images = doc.getElementsByTagName("img"),
-          i = images.length - 1;
-      do{
-        var image = images[i],
-            parent = image.parentNode,
-            neighbour = parent.previousSibling;
-            doc.body.insertBefore(image, parent);
-        parent.remove();
-      } while( i-- );
+
+    var moveImagesUpDom = function(){
+      var images = doc.getElementsByTagName("img");
+      if (images.length > 0) {
+        i = images.length - 1;
+        do{
+          var image = images[i],
+          parent = image.parentNode,
+          neighbour = parent.previousElementSibling;
+          doc.body.insertBefore(image, parent);
+          parent.remove();
+        } while( i-- );
+      }
     }(); // what does this do? I want my images to be at the root of the
          // document and easily hackable THANK YOU VERY MUCH computer
-    wrapper.className = "Post-imageContainer";
-    header.className = "Post-title";
-    wrapper.innerHTML = header.outerHTML;
-    if (subtitle.tagName.toLowerCase() === "blockquote") {
-      subtitle.className = "Post-subtitle";
-      wrapper.innerHTML = wrapper.innerHTML + subtitle.outerHTML;
+
+    var headers = doc.getElementsByTagName("h1"),
+        header = headers[0], // only pick the first one from the page just in case
+                             // there's more… there shouldn't be, we'll write it
+                             // in the submissions guideline
+        subtitle = null,
+        masthead = null,
+        provideWrapper = false;
+
+    if (headers.length > 0) {
+      var wrapper = doc.createElement("div");
+      provideWrapper = true;
+      wrapper.className = "Post-imageContainer";
+      header.className = "Post-title";
+      wrapper.innerHTML = header.outerHTML;
+      subtitle = header.nextElementSibling;
+      header.remove();
     };
 
-    if (subtitle.nextSibling.tagName.toLowerCase() === "img") {
-      subtitle.nextSibling.className = "Post-image";
-      wrapper.innerHTML = wrapper.innerHTML + subtitle.nextSibling.outerHTML;
+    if (subtitle != null) {
+      if (subtitle.tagName.toLowerCase() === "blockquote") {
+        subtitle.className = "Post-subtitle";
+        wrapper.innerHTML = wrapper.innerHTML + subtitle.outerHTML;
+        masthead = subtitle.nextElementSibling;
+      };
     }
-    doc.body.insertBefore(wrapper, doc.body.firstChild);
-    masthead.remove();
-    subtitle.nextSibling.remove(); // apparently calling masthead is just an empty paragraph tag now
-    subtitle.remove();
-    header.remove();
+
+    if (masthead != null && masthead.tagName.toLowerCase() === "img") {
+      subtitle.nextElementSibling.className = "Post-image";
+      wrapper.innerHTML = wrapper.innerHTML + subtitle.nextElementSibling.outerHTML;
+      subtitle.remove();
+      masthead.remove();
+    }
+
+    if (provideWrapper == true) {
+      doc.body.insertBefore(wrapper, doc.body.firstChild);
+    }
+
   }();
 
 }
